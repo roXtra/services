480148525fd81004a1f4c796b4628c0708f322f8
Johannes Preßmar <pressmar@rossmanith.com>
2019-07-24 11:32:54 +0200

    Remove createroxfile service

--- createroxfile/createroxfile-config.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import * as Semantic from "semantic-ui-react";
-
-export function createroxfileconfig() {
-  return <Semantic.Modal.Content>
-    <div><h1>Neues Dokument einstellen.</h1></div>
-  </Semantic.Modal.Content>;
-}
