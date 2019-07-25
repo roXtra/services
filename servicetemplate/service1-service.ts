4f263ce6d92359206f73711b0043a797bbf8f9a5
Johannes Preßmar <pressmar@rossmanith.com>
2019-07-25 15:50:37 +0200

    Add service template test tutorial

--- servicetemplate/service1-service.ts
+++ servicetemplate/service1-service.ts
@@ -1,29 +1,33 @@
 import * as PH from "processhub-sdk";
 
 export async function service1(environment: PH.ServiceTask.ServiceTaskEnvironment) {
-    // Get the instance to manipulate and add fields
-    let processObject: PH.Process.BpmnProcess = new PH.Process.BpmnProcess();
-    await processObject.loadXml(environment.bpmnXml);
-    let taskObject = processObject.getExistingTask(processObject.processId(), environment.bpmnTaskId);
-    let extensionValues = PH.Process.BpmnProcess.getExtensionValues(taskObject);
-    let config = extensionValues.serviceTaskConfigObject;
-    let fields = config.fields;
-    let instance = environment.instanceDetails;
-  
-    // Get field name of the corresponding field ID
-    let inputField = fields.find(f => f.key == "inputField").value;
-    let selectField = fields.find(f => f.key == "selectField").value;
-  
-    // Get the value of a selected field
-    let selectFieldValue = ((environment.instanceDetails.extras.fieldContents[selectField] as PH.Data.FieldValue).value as string);
+  // Get the instance to manipulate and add fields
+  let instance = await serviceLogic(environment);
 
-    // init new field
-    instance.extras.fieldContents["Neues Feld"] = {
-        value: "Ich bin neu",
-        type: "ProcessHubTextArea"
-      };
+  // init new field
+  instance.extras.fieldContents["Neues Feld"] = {
+    value: "Ich bin neu",
+    type: "ProcessHubTextArea"
+  };
 
-    // update the Instance with changes
-    await PH.Instance.updateInstance(environment.instanceDetails, environment.accessToken);
-    return true;
-  }
+  // update the Instance with changes
+  await PH.Instance.updateInstance(environment.instanceDetails, environment.accessToken);
+  return true;
+}
+
+// Extract the serviceLogic that testing is possible
+export async function serviceLogic(environment: PH.ServiceTask.ServiceTaskEnvironment) {
+  let processObject: PH.Process.BpmnProcess = new PH.Process.BpmnProcess();
+  await processObject.loadXml(environment.bpmnXml);
+  let taskObject = processObject.getExistingTask(processObject.processId(), environment.bpmnTaskId);
+  let extensionValues = PH.Process.BpmnProcess.getExtensionValues(taskObject);
+  let config = extensionValues.serviceTaskConfigObject;
+  let fields = config.fields;
+  let instance = environment.instanceDetails;
+  // Get field name of the corresponding field ID
+  let inputField = fields.find(f => f.key == "inputField").value;
+  let selectField = fields.find(f => f.key == "selectField").value;
+  // Get the value of a selected field
+  let selectFieldValue = ((environment.instanceDetails.extras.fieldContents[selectField] as PH.Data.FieldValue).value as string);
+  return instance;
+}
---
+++ servicetemplate/servicetemplate-test.roxtest.ts
@@ -0,0 +1,42 @@
+import { assert } from "chai";
+import * as PH from "processhub-sdk";
+import * as Service1 from "./service1-service"
+import * as Service2 from "./service2-service"
+
+describe("services", () => {
+    describe("servicetemplate", () => {
+
+        // create a mock service environment
+        function createEnvironment(bpmnXmlPath: string, bpmnTaskId: string, field1Value: string, field2Value: string): PH.ServiceTask.ServiceTaskEnvironment {
+            let env = PH.Test.createEmptyTestServiceEnvironment(bpmnXmlPath);
+            env.bpmnTaskId = bpmnTaskId;
+            env.fieldContents = { "Field1": { type: "ProcessHubNumber", value: field1Value }, "Field2": { type: "ProcessHubNumber", value: field2Value } };
+            env.instanceDetails.extras.fieldContents = env.fieldContents;
+
+            return env;
+        }
+
+        async function performService1Test(bpmnXmlPath: string, bpmnTaskId: string, feld1Value: string, feld2Value: string) {
+            let env = createEnvironment(bpmnXmlPath, bpmnTaskId, feld1Value, feld2Value);
+            await Service1.serviceLogic(env);
+
+            // then test if the environment changes as expected with asserts
+        }
+
+        async function performService2Test(bpmnXmlPath: string, bpmnTaskId: string, feld1Value: string, feld2Value: string) {
+            let env = createEnvironment(bpmnXmlPath, bpmnTaskId, feld1Value, feld2Value);
+            await Service2.serviceLogic(env);
+
+            // then test if the environment changes as expected with asserts
+        }
+
+        // execute tests with it()
+        it("execute service1 test_guid", async () => {
+            await performService1Test("path", "id", "field1", "field");
+        });
+
+        it("execute service 2 test_guid", async () => {
+            await performService2Test("path", "id", "field1", "field");
+        });
+    });
+});
