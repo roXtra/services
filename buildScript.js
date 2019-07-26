4036c6f399a2f2cd373dc6700838ff81aacbc7e6
Johannes Preßmar <pressmar@rossmanith.com>
2019-07-26 09:43:54 +0200

    Extended buildScript.js

--- buildScript.js
+++ buildScript.js
@@ -1,12 +1,18 @@
 const PowerShell = require("powershell");
 
+const exceptions = [
+    "node_modules",
+    "zipped_services",
+    "resources"
+]
+
 if (process.argv[2] == undefined) {
     let ps = new PowerShell("Get-ChildItem -Path ./ | where {($_.psiscontainer)} | ConvertTo-Json");
 
     ps.on("output", data => {
         data = JSON.parse(data);
         data.forEach(childDir => {
-            if (childDir.Name !== "node_modules" && childDir.Name !== "zipped_services") {
+            if (checkExceptions(childDir.Name)) {
                 new PowerShell("cd ./" + childDir.Name + ";" + "npm install; tsc; npm run copyandzip; cd..")
                     .on("output", data => {
                         console.log(data);
@@ -14,10 +20,25 @@ if (process.argv[2] == undefined) {
             }
         });
     });
-} else {
+} else if (checkExceptions(process.argv[2])) {
     new PowerShell("cd ./" + process.argv[2] + ";" + "npm install; tsc; npm run copyandzip; cd..")
         .on("output", data => {
             console.log(data);
         });
+} else {
+     console.error("The choosen directory is no service.");
+}
 
+function checkExceptions(childDirName) {
+    let bool = true;
+
+    for (let i in exceptions) {
+
+        if (childDirName === exceptions[i]) {
+            bool = false;
+            break;
+        }
+    }
+
+    return bool;
 }
