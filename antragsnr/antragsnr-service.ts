93053846fde34a2909aa83e9e008f7ef954704fa
Johannes Preßmar <pressmar@rossmanith.com>
2019-08-01 16:35:01 +0200

    Add antragsnr service

---
+++ antragsnr/antragsnr-service.ts
@@ -0,0 +1,52 @@
+
+import * as PH from "processhub-sdk";
+import { setError } from "./errorhelper";
+import { ProcessExtras } from "processhub-sdk/lib/process";
+
+export async function antragsnrAction(environment: PH.ServiceTask.ServiceTaskEnvironment): Promise<boolean> {
+  try {
+    const processObject: PH.Process.BpmnProcess = new PH.Process.BpmnProcess();
+    await processObject.loadXml(environment.bpmnXml);
+    const taskObject = processObject.getExistingTask(processObject.processId(), environment.bpmnTaskId);
+    const extensionValues = PH.Process.BpmnProcess.getExtensionValues(taskObject);
+
+    const config = extensionValues.serviceTaskConfigObject;
+    const fields = config.fields;
+    const targetField = fields.find(f => f.key == "targetfield").value;
+    
+    const processDetails = await PH.Process.loadProcess(environment.instanceDetails.processId, environment.instanceDetails.instanceId, ProcessExtras.ExtrasInstances, true, environment.accessToken);
+    serviceLogic(processDetails, environment, targetField);
+    await PH.Instance.updateInstance(environment.instanceDetails, environment.accessToken);
+  } catch (ex) {
+    await setError(environment, "(StartUpdateProcess): " + ex.toString());
+    return false;
+  }
+  return true;
+}
+
+export function serviceLogic(processDetails: PH.Process.ProcessDetails, environment: PH.ServiceTask.ServiceTaskEnvironment, targetField: string) {
+  const instances: PH.Instance.InstanceDetails[] = processDetails.extras.instances;
+  const instanceYear = environment.instanceDetails.createdAt.getFullYear();
+  const numberOfInstances = getNumberOfInstancesOfSpecificYear(instances, instanceYear);
+  const nr: string = (numberOfInstances < 10) ? instanceYear.toString() + "/0" + numberOfInstances : instanceYear.toString() + "/" + numberOfInstances;
+
+  const newValue: PH.Data.FieldValue = {
+    value: nr,
+    type: "ProcessHubTextInput",
+  };
+
+  environment.instanceDetails.extras.fieldContents[targetField] = newValue;
+}
+
+function getNumberOfInstancesOfSpecificYear(instances: PH.Instance.InstanceDetails[], year: number): number {
+  let instancesOfTheYear: PH.Instance.InstanceDetails[] = [];
+
+  for (let i = 0; i < instances.length; i++) {
+    const instanceYear = new Date(instances[i].createdAt).getFullYear();
+
+    if (year === instanceYear) {
+      instancesOfTheYear.push(instances[i]);
+    }
+  }
+  return instancesOfTheYear.length;
+}
---
+++ antragsnr/reportservice.roxtest.ts
@@ -0,0 +1,140 @@
+
+import * as PH from "processhub-sdk";
+import * as Antragsnr from "./antragsnr-service";
+import * as fs from "fs";
+import { assert } from "chai";
+
+describe("services", () => {
+    describe("servicetemplate", () => {
+
+        // create a mock service environment
+        function createEnvironment(bpmnXmlPath: string, bpmnTaskId: string): PH.ServiceTask.ServiceTaskEnvironment {
+            let env = PH.Test.createEmptyTestServiceEnvironment(fs.readFileSync(bpmnXmlPath, "utf8"));
+            env.bpmnTaskId = bpmnTaskId;
+            env.fieldContents = {};
+            env.instanceDetails.extras.fieldContents = env.fieldContents;
+            env.instanceDetails.createdAt = new Date("October 13, 2018 11:13:00");
+            return env;
+        }
+
+        async function performAntragsnrTest(bpmnXmlPath: string, bpmnTaskId: string, instances: PH.Instance.InstanceDetails[], tragetField: string): Promise<PH.ServiceTask.ServiceTaskEnvironment> {
+            let env = createEnvironment(bpmnXmlPath, bpmnTaskId);
+            let processDetails: PH.Process.ProcessDetails = {
+                processId: "",
+                workspaceId: "",
+                displayName: "",
+                description: "",
+                extras: { instances: instances },
+            }
+
+            await Antragsnr.serviceLogic(processDetails, env, tragetField);
+
+            return env;
+        }
+
+        it("execute antragsnr service test_8fd1ba08-f8a1-4caa-b685-27b3ee946038", async () => {
+            let targetFieldName = "target";
+
+            const instances: PH.Instance.InstanceDetails[] = [
+                // current instance
+                {
+                    instanceId: "",
+                    workspaceId: "",
+                    processId: "",
+                    extras: {},
+                    createdAt: new Date("October 13, 2018 11:13:00"),
+                },
+
+                // older instances
+                {
+                    instanceId: "",
+                    workspaceId: "",
+                    processId: "",
+                    extras: {},
+                    createdAt: new Date("October 13, 2014 11:13:00"),
+                },
+                {
+                    instanceId: "",
+                    workspaceId: "",
+                    processId: "",
+                    extras: {},
+                    createdAt: new Date("October 13, 2015 11:13:00"),
+                }
+            ];
+
+            let env = await performAntragsnrTest("./antragsnr/testfiles/antragsnr-test-process.bpmn", "ServiceTask_508AF9C8EEE3A181", instances, targetFieldName);
+
+            assert.equal(((env.fieldContents[targetFieldName] as PH.Data.FieldValue).value as string), "2018/01");
+        });
+
+        it("execute antragsnr service test_8fd1ba08-f8a1-4caa-b685-27b3ee946038", async () => {
+            let targetFieldName = "target";
+
+            const instances: PH.Instance.InstanceDetails[] = [
+                // current instance
+                {
+                    instanceId: "",
+                    workspaceId: "",
+                    processId: "",
+                    extras: {},
+                    createdAt: new Date("October 13, 2018 11:13:00"),
+                },
+
+                // older instances
+                {
+                    instanceId: "",
+                    workspaceId: "",
+                    processId: "",
+                    extras: {},
+                    createdAt: new Date("October 12, 2018 11:13:00"),
+                },
+                {
+                    instanceId: "",
+                    workspaceId: "",
+                    processId: "",
+                    extras: {},
+                    createdAt: new Date("October 13, 2015 11:13:00"),
+                }
+            ];
+
+            let env = await performAntragsnrTest("./antragsnr/testfiles/antragsnr-test-process.bpmn", "ServiceTask_508AF9C8EEE3A181", instances, targetFieldName);
+
+            assert.equal(((env.fieldContents[targetFieldName] as PH.Data.FieldValue).value as string), "2018/02");
+        });
+
+        it("execute antragsnr service test_8fd1ba08-f8a1-4caa-b685-27b3ee946038", async () => {
+            let targetFieldName = "target";
+
+            const instances: PH.Instance.InstanceDetails[] = [
+                // current instance
+                {
+                    instanceId: "",
+                    workspaceId: "",
+                    processId: "",
+                    extras: {},
+                    createdAt: new Date("October 13, 2018 11:13:00"),
+                },
+
+                // older instances
+                {
+                    instanceId: "",
+                    workspaceId: "",
+                    processId: "",
+                    extras: {},
+                    createdAt: new Date("October 12, 2018 11:13:00"),
+                },
+                {
+                    instanceId: "",
+                    workspaceId: "",
+                    processId: "",
+                    extras: {},
+                    createdAt: new Date("October 11, 2018 11:13:00"),
+                }
+            ];
+
+            let env = await performAntragsnrTest("./antragsnr/testfiles/antragsnr-test-process.bpmn", "ServiceTask_508AF9C8EEE3A181", instances, targetFieldName);
+
+            assert.equal(((env.fieldContents[targetFieldName] as PH.Data.FieldValue).value as string), "2018/03");
+        });
+    });
+});
