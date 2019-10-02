686afad4af7128b73f846e324a5a5ea475c9910e
Benjamin Caspari <caspari@rossmanith.com>
2019-10-02 12:25:11 +0200

    service to start an instance

---
+++ startinstance/startinstance-service.roxtest.ts
@@ -0,0 +1,61 @@
+import { startinstance } from "./startinstance-service";
+import * as PH from "processhub-sdk";
+import * as fs from "fs";
+import { expect } from "chai";
+
+describe("services", () => {
+  describe("roxfile", () => {
+    describe("startinstance-service", () => {
+
+      describe("startinstance", () => {
+        it("executes the service", async () => {
+
+          const valueFeld1 = "Feld1";
+          const valueFeld2 = 7;
+          const testAccessToken = "token";
+          let executeWasCalled = false;
+
+          const environment: PH.ServiceTask.ServiceTaskEnvironment = PH.Test.createEmptyTestServiceEnvironment(
+            fs.readFileSync("./testfiles/startservice.bpmn", "utf8")
+          );
+          environment.bpmnTaskName = "Start";
+          environment.bpmnTaskId = "ServiceTask_960AFDE95A570BF3";
+          environment.instanceDetails.extras.fieldContents = {
+            "Feld_1": {
+              type: "ProcessHubTextInput",
+              value: valueFeld1,
+            },
+            "Feld_2": {
+              type: "ProcessHubNumber",
+              value: valueFeld2,
+            }
+          };
+
+          // eslint-disable-next-line @typescript-eslint/unbound-method, @typescript-eslint/require-await
+          environment.roxApi.getRoxtraTokenByUserId = async (userId: string): Promise<string> => {
+            // Must match the value defined in startservice.bpmn
+            expect(userId).to.equal("7133");
+            return testAccessToken;
+          };
+
+          // eslint-disable-next-line @typescript-eslint/unbound-method, @typescript-eslint/require-await
+          environment.instances.executeInstance = async (processId, instance, _startEventId, accessToken): Promise<string> => {
+            executeWasCalled = true;
+            expect(accessToken).to.equal(testAccessToken);
+            // Must match the value defined in startservice.bpmn
+            expect(processId).to.equal("E431DD73D9B0EDEB");
+            // Make sure field contents were set
+            expect((instance.extras.fieldContents["Feld_1"] as PH.Data.FieldValue).value).to.equal(valueFeld1);
+            expect((instance.extras.fieldContents["Feld_2"] as PH.Data.FieldValue).value).to.equal(valueFeld2);
+            return instance.instanceId;
+          };
+
+          const result = await startinstance(environment);
+          expect(result).to.equal(true);
+          expect(executeWasCalled).to.equal(true);
+
+        });
+      });
+    });
+  });
+});
---
+++ startinstance/startinstance-service.ts
@@ -0,0 +1,60 @@
+import * as PH from "processhub-sdk";
+
+async function getServiceTaskConfig(environment: PH.ServiceTask.ServiceTaskEnvironment): Promise<{
+  workspaceAndProcessId: string;
+  fields: string[];
+  executingUserId: string;
+}> {
+  const processObject: PH.Process.BpmnProcess = new PH.Process.BpmnProcess();
+  await processObject.loadXml(environment.bpmnXml);
+  const taskObject = processObject.getExistingTask(processObject.processId(), environment.bpmnTaskId);
+  const extensionValues = PH.Process.BpmnProcess.getExtensionValues(taskObject);
+  const config = extensionValues.serviceTaskConfigObject;
+  const fields = config.fields;
+  const workspaceAndProcessId = fields.find(f => f.key === "processId").value;
+  const fieldsString = fields.find(f => f.key === "fields");
+  const executingUserId = fields.find(f => f.key === "executingUserId").value;
+  return {
+    executingUserId,
+    workspaceAndProcessId,
+    fields: (fieldsString.value && fieldsString.value.length > 0) ? JSON.parse(fieldsString.value) : []
+  };
+}
+
+export async function startinstance(environment: PH.ServiceTask.ServiceTaskEnvironment): Promise<boolean> {
+
+  try {
+    const { workspaceAndProcessId, fields, executingUserId } = await getServiceTaskConfig(environment);
+    const workspaceId = workspaceAndProcessId.split("/")[0];
+    const processId = workspaceAndProcessId.split("/")[1];
+
+    const accessToken = await environment.roxApi.getRoxtraTokenByUserId(executingUserId);
+
+    const oldFieldContents = environment.instanceDetails.extras.fieldContents;
+    const newFieldContents: PH.Data.FieldContentMap = {};
+    for (const fieldName of fields) {
+      newFieldContents[fieldName] = oldFieldContents[fieldName];
+    }
+
+    const newInstance: PH.Instance.InstanceDetails = {
+      instanceId: PH.Tools.createId(),
+      workspaceId,
+      processId,
+      extras: {
+        instanceState: undefined,
+        fieldContents: newFieldContents,
+      }
+    };
+
+    await environment.instances.executeInstance(processId, newInstance, undefined, accessToken);
+    return true;
+  } catch (ex) {
+    console.error(ex);
+    environment.instanceDetails.extras.fieldContents["ErrorField"] = {
+      type: "ProcessHubTextInput",
+      value: PH.tl("Fehler beim Ausführen des Services: ") + JSON.stringify(ex),
+    };
+    await environment.instances.updateInstance(environment.instanceDetails);
+    return false;
+  }
+}
