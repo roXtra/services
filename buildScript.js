316c16c5f91a9c70d724c3fee7188453c1fa0d93
Claudius Eisele <eisele@rossmanith.com>
2019-10-07 11:35:12 +0200

    Updated SDK to v8.10.0-7

--- buildScript.js
+++ buildScript.js
@@ -1,7 +1,7 @@
 const dirTree = require('directory-tree');
 const { execSync } = require('child_process');
 
-const processHubSDKVersion = 'v8.10.0-6';
+const processHubSDKVersion = 'v8.10.0-7';
 
 const childProcessStdioOptions = [0, 1, 2];
 const childProcessTimeout = 300000;
