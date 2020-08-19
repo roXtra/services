688a9efec891bd1a6a7954808dba5846e90d87c2
Claudius Eisele <eisele@roxtra.com>
2020-08-19 08:28:30 +0200

    Update SDK

--- buildScript.js
+++ buildScript.js
@@ -1,7 +1,7 @@
 const dirTree = require('directory-tree');
 const { execSync } = require('child_process');
 
-const processHubSDKVersion = 'v8.30.0';
+const processHubSDKVersion = 'v8.31.0';
 
 const childProcessStdioOptions = [0, 1, 2];
 const childProcessTimeout = 300000;
