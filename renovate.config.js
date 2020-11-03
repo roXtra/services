6812b8df5e7c31d69ce16d685b1e229c1b83ca10
Claudius Eisele <eisele@roxtra.com>
2020-11-03 14:29:06 +0100

    Remove Renovate regex manager workaround for updating github actions
    * Renovate now supports github-actions tags since https://github.com/renovatebot/renovate/releases/tag/23.55.0

--- renovate.config.js
+++ renovate.config.js
@@ -2,20 +2,14 @@ const branchName = 'auto-dep-update';
 
 module.exports = {
   branchPrefix: `${branchName}/`,
-  enabledManagers: ['github-actions', 'regex', 'npm'],
+  enabledManagers: ['github-actions', 'npm'],
   gitAuthor: 'Dependency Bot <devbot@roxtra.com>',
   logLevel: 'info',
   onboarding: true,
   onboardingBranch: `${branchName}/configure`,
   platform: 'github',
   schedule: ["after 6am and before 5pm on Wednesday"],
-  regexManagers: [
-    {
-      datasourceTemplate: 'github-tags',
-      fileMatch: ['^\\.github/workflows/[^/]+\\.ya?ml$'],
-      matchStrings: ['uses: (?<depName>.*?)@(?<currentValue>.*?)\\s'],
-    },
-  ],
+  regexManagers: [],
   repositories: [
     'roXtra/services',
   ],
