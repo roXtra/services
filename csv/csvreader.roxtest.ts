4f2e41791bf310f171a3eac063bcaccbaf686ac9
BCaspari <caspari@rossmanith.com>
2021-05-20 15:47:17 +0200

    ^csv tests use webpack bundle

--- csv/csvreader.roxtest.ts
+++ csv/csvreader.roxtest.ts
@@ -1,1 +1,14 @@
 // Tests for csvreader
+import { expect } from "chai";
+import { csvreaderConfig, csvreader } from "./main";
+
+describe("services", () => {
+  describe("csv", () => {
+    describe("bundle test", () => {
+      it("should check for bundled methods", () => {
+        expect(typeof csvreader).to.equal("function");
+        expect(typeof csvreaderConfig).to.equal("function");
+      });
+    });
+  });
+});
