a5ffea1a32ab74b9f22bc44d9a5d45eb2ceab59c
Alexander Krinke <alexander.krinke@roxtra.com>
2024-07-10 16:55:50 +0200

    added tseslint rules and removed non null assertion

--- eslint.config.js
+++ eslint.config.js
@@ -58,7 +58,6 @@ export default tseslint.config(
       },
     },
     rules: {
-      "@typescript-eslint/no-non-null-assertion": "error",
       "linebreak-style": ["error", "unix"],
       "no-cond-assign": "error",
       "capitalized-comments": [
@@ -74,6 +73,7 @@ export default tseslint.config(
       "no-trailing-spaces": "error",
       "no-unsafe-finally": "error",
       "no-var": "error",
+      "default-param-last": "off",
       eqeqeq: [
         "error",
         "always",
@@ -177,7 +177,6 @@ export default tseslint.config(
       "@typescript-eslint/no-unsafe-member-access": "warn",
       "@typescript-eslint/no-unsafe-call": "warn",
       "@typescript-eslint/no-unsafe-return": "warn",
-
       "@typescript-eslint/triple-slash-reference": "off",
       "@typescript-eslint/explicit-function-return-type": [
         "off",
@@ -185,6 +184,9 @@ export default tseslint.config(
           allowExpressions: true,
         },
       ],
+      "@typescript-eslint/no-non-null-assertion": "error",
+      "@typescript-eslint/no-useless-empty-export": "error",
+      "@typescript-eslint/default-param-last": "error",
     },
   },
   /**
